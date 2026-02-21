c1da42d85ed332a85979d07b494cd7c31e962a21
hermanzands <hermanzands@gmail.com>
2026-02-21 16:55:53 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'wrapgod-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Only cache GET requests, pass API calls straight through
+  if (e.request.method !== 'GET' || e.request.url.includes('/api/')) {
+    return;
+  }
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
